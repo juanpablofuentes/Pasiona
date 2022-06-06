02c05d0c2e86d955407ab64d649f0307254d3aaf
Juan Pablo <incidencias@gmail.com>
2022-06-06 16:25:16 +0200

    Ejemplos Array

---
+++ Ejemplos01/EjemplosArray/Program.cs
@@ -0,0 +1,87 @@
+﻿string[] cadenas = { "hola", "que", "tal","amigo" };
+
+var cads= new[]  { "hola", "que", "tal" };
+
+//Error en compilación, no hay un tipo adecuado
+//var error = new[] { "hola", 7, "tal" };
+
+object[] variado = { "hola", 4, 5.5, new DateTime(2022,6,4) };
+
+foreach(var c in variado)
+{
+    Console.WriteLine(c);
+    Console.WriteLine(c.GetType());
+
+}
+
+Console.WriteLine(String.Join(",",cadenas));
+Array.Sort(cadenas);
+Console.WriteLine(String.Join(",", cadenas));
+Array.Reverse(cadenas);
+Console.WriteLine(String.Join(",", cadenas));
+Array.Fill(cadenas, "foo");
+Console.WriteLine(String.Join(",", cadenas));
+
+
+int[,] myMatrix;
+myMatrix = new int[3, 4];
+// Rellenar (3 * 4) array.
+for (int i = 0; i < 3; i++)
+{
+    for (int j = 0; j < 4; j++)
+    {
+        myMatrix[i, j] = i * j;
+    }
+}
+
+int[,] otra = { { 1, 2 }, { 3, 4 } };
+
+Console.WriteLine(myMatrix.Length);
+Console.WriteLine(myMatrix.Rank);
+
+
+printArray(myMatrix);
+printArray(otra);
+
+printArrayPretty(myMatrix);
+printArrayPretty(otra);
+
+
+int[] numeros = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+foreach(int i in numeros[2..5])
+{
+    Console.WriteLine(i);
+}
+
+Range rango = 2..5;
+
+foreach (int i in numeros[rango])
+{
+    Console.WriteLine(i);
+}
+
+foreach (int i in numeros[3..^1])
+{
+    Console.WriteLine(i);
+}
+
+
+void printArray(int[,] tabla)
+{
+    foreach(var c in tabla)
+    {
+            Console.Write(c+",");
+    }
+}
+void printArrayPretty(int[,] tabla)
+{
+    for(int i = 0; i < tabla.GetLength(0); i++)
+    {
+        for(int j = 0; j < tabla.GetLength(1); j++)
+        {
+            Console.Write(tabla[i, j]+",");
+        }
+        Console.WriteLine();
+    }
+}
--- Ejemplos01/EjemplosCadenas/Program.cs
+++ Ejemplos01/EjemplosCadenas/Program.cs
@@ -1,4 +1,5 @@
-﻿string cadena = "   Hola que tal   ";
+﻿
+string cadena = "   Hola que tal   ";
 Console.WriteLine(cadena);
 Console.WriteLine(cadena.Trim());
 
@@ -21,8 +22,80 @@ Console.WriteLine(cadena.Remove(5,3));
 Console.WriteLine(cadena[4]);
 Console.WriteLine(cadena.Substring(4,2));
 
-String[] palabras = cadena.Split(" ");
+string[] palabras = cadena.Split(" ");
 foreach(string palabra in palabras)
 {
     Console.WriteLine(palabra);
+}
+
+string s1 = "Adios";
+string s2 = "ADIOS";
+Console.WriteLine("Por defecto: s1={0},s2={1}s1.Equals(s2): {2}", s1, s2, s1.Equals(s2));
+Console.WriteLine("Case  insensitive: s1.Equals(s2, StringComparison.OrdinalIgnoreCase): {0}", s1.Equals(s2, StringComparison.OrdinalIgnoreCase));
+
+Console.WriteLine(FromRainbow("Red"));
+
+(String letra, String resto) = Trocear("hola", 0);
+
+string[] foo=Anagrama("pera");
+foreach(var el in foo)
+{
+    Console.WriteLine(el);
+}
+
+string[] Anagrama(string cadena)
+{
+    int longitud = cadena.Length;
+    int tamany = Factorial(longitud);
+    string[] res = new string[tamany];
+    if (longitud == 1)
+    {
+        res[0] = cadena;
+    }
+    else
+    {
+        int indice = 0;
+        for(int i = 0; i < longitud; i++)
+        {
+            (string letra, string resto)=Trocear(cadena,i);
+            string[] temp = Anagrama(resto);
+            for(int j=0;j<temp.Length; j++)
+            {
+                res[indice++] = letra+temp[j];
+            }
+        }
+    }
+    return res;
+}
+
+(string, string) Trocear(string cadena, int pos)
+{
+    String letra = cadena.Substring(pos, 1);
+    String resto = cadena.Remove(pos, 1);
+    return (letra, resto);
+}
+
+int Factorial(int num)
+{
+    int res = 1;
+    for(int i = 1; i <= num; i++)
+    {
+        res*=i;
+    }
+    return res;
+}
+
+string FromRainbow(string colorBand)
+{
+    return colorBand switch
+    {
+        "Red" => "#FF0000",
+        "Orange" => "#FF7F00",
+        "Yellow" => "#FFFF00",
+        "Green" => "#00FF00",
+        "Blue" => "#0000FF",
+        "Indigo" => "#4B0082",
+        "Violet" => "#9400D3",
+        _ => "#FFFFFF",
+    };
 }
