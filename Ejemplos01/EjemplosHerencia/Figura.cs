0467ceea8ec0e3e72be15b7a6258247f0ded1110
Juan Pablo <incidencias@gmail.com>
2022-06-08 19:46:37 +0200

    Clases, herencias, interfaces

---
+++ Ejemplos01/EjemplosHerencia/Figura.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemplosHerencia
+{
+    internal abstract class Figura
+    {
+        public string Nombre { get; set; }
+
+        public abstract float area();
+    }
+
+    class Hexagono : Figura, IDraw
+    {
+
+    }
+
+    class Pentagono : Figura, IFullDraw
+    {
+
+    }
+}
