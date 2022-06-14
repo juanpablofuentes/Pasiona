c214be70c84ec8d944bceaff3f939238b7a1624e
Juan Pablo Fuentes <incidencias@gmail.com>
2022-06-14 12:57:05 +0200

    SOLID

---
+++ Ejemplos01/PiedraPapelTijerasSOLID_03/Program.cs
@@ -0,0 +1,7 @@
+﻿using Juego_v3;
+
+Jugador eva = new Jugador("Eva", new CPU());
+
+Jugador juan = new Jugador("Juan", new CPU());
+Juego ppt = new Juego(eva, juan, new JuegoPPTLS());
+Console.WriteLine(ppt.jugar());
