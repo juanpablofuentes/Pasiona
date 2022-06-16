a08e4a06646e90be0fdce2feda1e1078dc966d47
Juan Pablo Fuentes <incidencias@gmail.com>
2022-06-16 12:14:51 +0200

    Colecciones

---
+++ Ejemplos01/PiedraPapelTijerasSOLID_03/JuegoPPTLSGE.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Juego_v3
+{
+    class JuegoPPTLSGE : ILogica
+    {
+        private string[] jugadas = new string[] { "piedra", "papel", "tijeras", "spock", "lagarto", "jarra", "edans" };
+
+        public int comprobar(string jugada1, string jugada2)
+        {
+            int[][] matriz = { new int[]  {2,4,5 }, new int[] { 0,6,3 },
+                               new int[] { 6,1,4},  new int[] { 5,0,2},
+                        new int[] { 1,5,3 }, new int[] { 5,1,2 },new int[]  {0,4,3 }  };
+
+            int pos1 = Array.IndexOf(jugadas, jugada1.ToLower());
+            int pos2 = Array.IndexOf(jugadas, jugada2.ToLower());
+            if (matriz[pos1].Contains(pos2)) return 1;
+            if (matriz[pos2].Contains(pos1)) return 2;
+            return 0;
+        }
+
+        string[] ILogica.validas()
+        {
+            return this.jugadas;
+        }
+    }
+    
+    
+}
---
+++ Ejemplos01/PiedraPepelTijera_04/Program.cs
@@ -0,0 +1,7 @@
+﻿using Juego_v3;
+
+Jugador eva = new Jugador("Eva", new CPU());
+Jugador juan = new Jugador("Juan", new CPU());
+Juego ppt = new Juego(eva, juan, new JuegoPPTLS(), new Archivo());
+for (int i = 0; i < 5; i++)
+    ppt.jugar();
