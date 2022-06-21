e8af1a9c930a852004206f8855eaafe032009c4c
Juan Pablo Fuentes <incidencias@gmail.com>
2022-06-21 13:15:29 +0200

    Indexadores

---
+++ Ejemplos01/Indexadores/Program.cs
@@ -0,0 +1,16 @@
+﻿using Indexadores;
+
+Clase Mates = new Clase("Matemáticas");
+
+Mates.Alumnos.Add("Eva");
+Mates.Alumnos.Add("Ana");
+Mates.Alumnos.Add("Rosa");
+
+//Mates[1] = "Juan";
+Mates.Alumnos[1] = "Juan";
+
+//Mates[10] = "Pep";
+
+
+Console.WriteLine(Mates);
+Console.WriteLine(Mates[3]);
