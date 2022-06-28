670249dafd79d00dbbf6c0655536676f39e90286
Juan Pablo Fuentes <incidencias@gmail.com>
2022-06-28 14:50:26 +0200

    Algoritmia

---
+++ Ejemplos01/Algoritmia/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Algoritmia;
+
+
+List<String> ganador(List<Jugador> jugadores)
+{
+    // Ordenar arrays de mayor a menor
+
+    // Recorrer el array de valores desde el principio hasta el final
+
+    // Buscar él o los elementos de la lista con valor más alto
+
+    // Si el elemento es único ya lo tenemos, ese es el ganador
+    // Si hay varios tenemos que seguir buscando en la siguiente posición
+    // Los que no han ganado o los eliminamos o los ponemos a 0
+
+    // Si hemos llegado a final del bucle es porque los que quedan son los ganadores
+
+
+}
