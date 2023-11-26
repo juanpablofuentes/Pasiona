0cec36e8d1d5d27f9f85e9e7950c2170fab1b551
Juan Pablo <incidencias@gmail.com>
2023-11-26 11:10:47 +0100

    Numeros complejos

---
+++ Ejemplos01/NumerosComplejos/NumeroComplejo.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumerosComplejos
+{
+    internal class NumeroComplejo:IComparable<NumeroComplejo>
+    {
+        public double ParteReal { get; set; }
+        public double ParteImaginaria { get; set; }
+
+        public double Modulo { get=> Math.Sqrt(ParteReal * ParteReal + ParteImaginaria * ParteImaginaria);}
+        // Constructor
+        public NumeroComplejo(double parteReal, double parteImaginaria)
+        {
+            ParteReal = parteReal;
+            ParteImaginaria = parteImaginaria;
+        }
+
+        
+
+        // Sobrecarga del operador de suma
+        public static NumeroComplejo operator +(NumeroComplejo a, NumeroComplejo b)
+        {
+            return new NumeroComplejo(a.ParteReal + b.ParteReal, a.ParteImaginaria + b.ParteImaginaria);
+        }
+
+        // Sobrecarga del operador de resta
+        public static NumeroComplejo operator -(NumeroComplejo a, NumeroComplejo b)
+        {
+            return new NumeroComplejo(a.ParteReal - b.ParteReal, a.ParteImaginaria - b.ParteImaginaria);
+        }
+
+        // Sobrecarga del operador de multiplicación
+        public static NumeroComplejo operator *(NumeroComplejo a, NumeroComplejo b)
+        {
+            double real = a.ParteReal * b.ParteReal - a.ParteImaginaria * b.ParteImaginaria;
+            double imaginaria = a.ParteReal * b.ParteImaginaria + a.ParteImaginaria * b.ParteReal;
+            return new NumeroComplejo(real, imaginaria);
+        }
+
+        // Sobrecarga del operador de división
+        public static NumeroComplejo operator /(NumeroComplejo a, NumeroComplejo b)
+        {
+            double denominador = b.ParteReal * b.ParteReal + b.ParteImaginaria * b.ParteImaginaria;
+            double real = (a.ParteReal * b.ParteReal + a.ParteImaginaria * b.ParteImaginaria) / denominador;
+            double imaginaria = (a.ParteImaginaria * b.ParteReal - a.ParteReal * b.ParteImaginaria) / denominador;
+            return new NumeroComplejo(real, imaginaria);
+        }
+
+       
+
+        // Sobrecarga de operadores de comparación
+        public static bool operator <(NumeroComplejo a, NumeroComplejo b)
+        {
+            return a.Modulo < b.Modulo;
+        }
+
+        public static bool operator >(NumeroComplejo a, NumeroComplejo b)
+        {
+            return a.Modulo > b.Modulo;
+        }
+
+        public static bool operator <=(NumeroComplejo a, NumeroComplejo b)
+        {
+            return a.Modulo <= b.Modulo;
+        }
+
+        public static bool operator >=(NumeroComplejo a, NumeroComplejo b)
+        {
+            return a.Modulo >= b.Modulo;
+        }
+
+        // Sobrecarga de operadores de igualdad
+        public static bool operator ==(NumeroComplejo a, NumeroComplejo b)
+        {
+            return a.ParteReal == b.ParteReal && a.ParteImaginaria == b.ParteImaginaria;
+        }
+
+        public static bool operator !=(NumeroComplejo a, NumeroComplejo b)
+        {
+            return !(a == b);
+        }
+
+        // Sobrecarga de operadores unarios
+        public static NumeroComplejo operator -(NumeroComplejo a)
+        {
+            return new NumeroComplejo(-a.ParteReal, -a.ParteImaginaria);
+        }
+        public override string ToString()
+        {
+            return ParteReal+" + "+ParteImaginaria+"i";
+        }
+
+       
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            NumeroComplejo other = (NumeroComplejo)obj;
+            return this == other;
+        }
+        public int CompareTo(NumeroComplejo? other)
+        {
+            return this.Modulo.CompareTo(other?.Modulo);
+        }
+    }
+}
---
+++ Ejemplos01/NumerosComplejos/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace NumerosComplejos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            NumeroComplejo a = new NumeroComplejo(1, 2);
+            NumeroComplejo b = new NumeroComplejo(2,1); 
+            Console.WriteLine(a.CompareTo(b));
+            Console.WriteLine(a.Modulo);
+            Console.WriteLine(a>=b);
+            Console.WriteLine(a > b);
+            List<NumeroComplejo> lista= new List<NumeroComplejo>() { new NumeroComplejo(3, 4), new NumeroComplejo(7, 7), new NumeroComplejo(3, 1), new NumeroComplejo(2, 3), new NumeroComplejo(1, 1) };
+            Console.WriteLine(String.Join(",",lista));
+            lista.Sort();
+            Console.WriteLine(String.Join(",", lista));
+            NumeroComplejo c = new NumeroComplejo(2, 1);
+
+            Dictionary<NumeroComplejo, int> numeros=new Dictionary<NumeroComplejo, int>();
+            numeros.Add(c, 1);
+            numeros.Add(b, 2);
+            Console.WriteLine(numeros[c]);
+        }
+    }
+}
