cc9504aa941a903c881873b2a1330fa551ca8b21
Juan Pablo <incidencias@gmail.com>
2023-11-29 14:13:03 +0100

    ado

--- Ejemplos01/ADOEjemplo/Program.cs
+++ Ejemplos01/ADOEjemplo/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Data.SqlClient;
+using System.Data;
 
 namespace ADOEjemplo
 {
@@ -16,7 +17,26 @@ namespace ADOEjemplo
             command.Parameters.AddWithValue("@nombre", "Ana Pi");
             
             command.ExecuteNonQuery();
-            
+
+            SqlCommand preparado = new SqlCommand(sql, connection);
+            preparado.Parameters.Add("@nombre", SqlDbType.VarChar,50);
+            preparado.Prepare();
+            for(int i = 0; i < 10; i++)
+            {
+                command.Parameters["@nombre"].Value= "Alumno "+i;
+                command.ExecuteNonQuery();
+            }
+
+            SqlCommand lectura=new SqlCommand("select * from alumno", connection);
+            SqlDataReader sqlDataReader = lectura.ExecuteReader();
+            while(sqlDataReader.Read())
+            {
+                Console.WriteLine(sqlDataReader.GetInt32(0));
+                Console.WriteLine(sqlDataReader["id"]);
+                Console.WriteLine(sqlDataReader.GetString(1));
+                Console.WriteLine(sqlDataReader["nombre"]);
+
+            }
 
             connection.Close();
         }
