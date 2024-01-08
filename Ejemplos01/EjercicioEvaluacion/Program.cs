7e2a0670ec2b717a6a2eb21d100a584975a81ffb
Juan pablo <incidencias@hmail.com>
2024-01-08 16:58:13 +0100

    Ejercicio

--- Ejemplos01/EjercicioEvaluacion/Program.cs
+++ Ejemplos01/EjercicioEvaluacion/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Crear una clase CuentaCorriente que tenga las siguientes propiedades
+            // Titular, Saldo, Numero, Movimientos
+            // Titular y Numero son de tipo String y se pueden leer y modificar
+            // Saldo es de solo lectura
+            // Movimientos es una lista de tipo decimal
+            // Los movimientos solo se pueden añadir
+            // El Saldo se calcula sumando los importes de todos los movimientos
+            // Sobrecargar el operador + y el operador - para
+            // Que añadan un movimiento con ese valor en positivo o en negativo
+            // juan.pablo@asociacionaepi.es
+
         }
     }
 }
